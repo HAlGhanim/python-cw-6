9aab70378d7cf7b032251344e925b19637fabc2d
HAlGhanim <imzaji98@gmail.com>
2022-12-10 19:35:45 +0300

    cw6

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# write your code here
+# الجزء الاول
+person = {
+    "name":"Humoud",
+    "age":"23",
+    "hobbies": ["Coding", "Gaming", "Boxing"]
+}
+
+print(person["name"])
+print(person["age"])
+
+# الجزء الثاني
+person["age"] = "25"
+person["country"] = "Kuwait"
+print(person)
+print(len(person))
+
+# الجزء الثالث
+person["hobbies"] = "Coding", "Gaming", "Boxing", "Sleeping"
+def check_hobbies(person):
+    if len(person["hobbies"]) > 3:
+        print("WOW YOU ARE AMAZING")
+check_hobbies(person)
